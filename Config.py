70714227471ba069913df27eac30579d92887ef2
notreallyshikhar <63592764+notreallyshikhar@users.noreply.github.com>
2021-06-25 07:58:32 +0000

    Update Config.py

--- Config.py
+++ Config.py
@@ -4,6 +4,7 @@ from dotenv import load_dotenv
 load_dotenv()
 
 STRING = getenv("SESSION")
+STRING2 = getenv("SESSION2")
 API_ID = getenv("API_ID")
 API_HASH = getenv("API_HASH")
 BIO_MESSAGE = getenv("BIO")
