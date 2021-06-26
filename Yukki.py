867bd7e51803e9b5b3027fdb377d935b320b14b3
notreallyshikhar <63592764+notreallyshikhar@users.noreply.github.com>
2021-06-26 06:55:28 +0000

    Update Yukki.py

--- Yukki.py
+++ Yukki.py
@@ -7,7 +7,7 @@ from telethon.sessions import StringSession
 from telethon.tl.functions.account import UpdateProfileRequest
 from Config import STRING, SUDO, BIO_MESSAGE, API_ID, API_HASH, STRING2, STRING3, STRING4 ,STRING5
 import asyncio
-from pyrogram import idle
+import telethon.utils
 a = API_ID
 b = API_HASH
 smex = STRING
@@ -15,6 +15,16 @@ smexx = STRING2
 smexxx = STRING3
 smexxxx = STRING4
 smexxxxx = STRING5
+sixth = STRING6
+seven = STRING7
+eight = STRING8
+ninth = STRING9
+tenth = STRING10
+
+
+SMEX_USERS = []
+SMEX_USERS.append(SUDO)
+
 
 if smex:
     session_name = str(smex)
@@ -23,6 +33,9 @@ if smex:
     try:
         print("Booting Up The Client 1")
         idk.start()
+        botme = await idk.get_me()
+        botid = telethon.utils.get_peer_id(botme)
+        SMEX_USERS.append(botid)
     except Exception as e:
         idk = "smex"
         print(e)
@@ -43,6 +56,9 @@ if smexx:
     try:
         print("Booting Up The Client 2")
         ydk.start()
+        botme = await ydk.get_me()
+        botid = telethon.utils.get_peer_id(botme)
+        SMEX_USERS.append(botid)
     except Exception as e:
         print(e)
         pass
@@ -63,6 +79,9 @@ if smexxx:
     try:
         print("Booting Up The Client 3")
         wdk.start()
+        botme = await wdk.get_me()
+        botid = telethon.utils.get_peer_id(botme)
+        SMEX_USERS.append(botid)
     except Exception as e:
         print(e)
         pass
@@ -83,6 +102,9 @@ if smexxxx:
     try:
         print("Booting Up The Client 4")
         hdk.start()
+        botme = await hdk.get_me()
+        botid = telethon.utils.get_peer_id(botme)
+        SMEX_USERS.append(botid)
     except Exception as e:
         print(e)
         pass
@@ -103,6 +125,9 @@ if smexxxxx:
     try:
         print("Booting Up The Client 5")
         sdk.start()
+        botme = await sdk.get_me()
+        botid = telethon.utils.get_peer_id(botme)
+        SMEX_USERS.append(botid)
     except Exception as e:
         print(e)
         pass
@@ -115,16 +140,155 @@ else:
         sdk.start()
     except Exception as e:
         pass
-          
-@idk.on(events.NewMessage(incoming=True, pattern=".bio"))
-async def biook(e):
-    if e.sender_id in SUDO:
-        text = "Bio Applied Successfully"
-        await e.reply(text, parse_mode=None, link_preview=None )
-        await idk(UpdateProfileRequest(
-            about=f'{BIO_MESSAGE}'
-        ))
+                  
+if sixth:
+    session_name = str(sixth)
+    print("String 6 Found")
+    sdk = TelegramClient(StringSession(session_name), a, b)
+    try:
+        print("Booting Up The Client 6")
+        adk.start()
+        botme = await adk.get_me()
+        botid = telethon.utils.get_peer_id(botme)
+        SMEX_USERS.append(botid)
+    except Exception as e:
+        print(e)
+        pass
+else:
+    print("Session 6 not Found")
+    pass
+    session_name = "startup"
+    adk = TelegramClient(session_name, a, b)
+    try:
+        adk.start()
+    except Exception as e:
+        pass
 
+if seven:
+    session_name = str(seven)
+    print("String 7 Found")
+    sdk = TelegramClient(StringSession(session_name), a, b)
+    try:
+        print("Booting Up The Client 7")
+        bdk.start()
+        botme = await bdk.get_me()
+        botid = telethon.utils.get_peer_id(botme)
+        SMEX_USERS.append(botid)
+    except Exception as e:
+        print(e)
+        pass
+else:
+    print("Session 7 not Found")
+    pass
+    session_name = "startup"
+    bdk = TelegramClient(session_name, a, b)
+    try:
+        bdk.start()
+    except Exception as e:
+        pass    
+    
+    
+ if eight:
+    session_name = str(eight)
+    print("String 8 Found")
+    sdk = TelegramClient(StringSession(session_name), a, b)
+    try:
+        print("Booting Up The Client 8")
+        cdk.start()
+        botme = await cdk.get_me()
+        botid = telethon.utils.get_peer_id(botme)
+        SMEX_USERS.append(botid)
+    except Exception as e:
+        print(e)
+        pass
+else:
+    print("Session 8 not Found")
+    pass
+    session_name = "startup"
+    cdk = TelegramClient(session_name, a, b)
+    try:
+        cdk.start()
+    except Exception as e:
+        pass   
+    
+    
+ if ninth:
+    session_name = str(ninth)
+    print("String 9 Found")
+    sdk = TelegramClient(StringSession(session_name), a, b)
+    try:
+        print("Booting Up The Client 9")
+        ddk.start()
+        botme = await ddk.get_me()
+        botid = telethon.utils.get_peer_id(botme)
+        SMEX_USERS.append(botid)
+    except Exception as e:
+        print(e)
+        pass
+else:
+    print("Session 9 not Found")
+    pass
+    session_name = "startup"
+    ddk = TelegramClient(session_name, a, b)
+    try:
+        ddk.start()
+    except Exception as e:
+        pass   
+    
+  
+if tenth:
+    session_name = str(tenth)
+    print("String 10 Found")
+    sdk = TelegramClient(StringSession(session_name), a, b)
+    try:
+        print("Booting Up The Client 10")
+        edk.start()
+        botme = await edk.get_me()
+        botid = telethon.utils.get_peer_id(botme)
+        SMEX_USERS.append(botid)
+    except Exception as e:
+        print(e)
+        pass
+else:
+    print("Session 10 not Found")
+    pass
+    session_name = "startup"
+    edk = TelegramClient(session_name, a, b)
+    try:
+        edk.start()
+    except Exception as e:
+        pass 
+    
+    
+@idk.on(events.NewMessage(incoming=True, pattern=".bio (.*)"))
+@ydk.on(events.NewMessage(incoming=True, pattern=".bio (.*)"))
+@wdk.on(events.NewMessage(incoming=True, pattern=".bio (.*)"))
+@hdk.on(events.NewMessage(incoming=True, pattern=".bio (.*)"))
+@sdk.on(events.NewMessage(incoming=True, pattern=".bio (.*)"))
+@sixth.on(events.NewMessage(incoming=True, pattern=".bio (.*)"))
+@seven.on(events.NewMessage(incoming=True, pattern=".bio (.*)"))
+@eight.on(events.NewMessage(incoming=True, pattern=".bio (.*)"))
+@ninth.on(events.NewMessage(incoming=True, pattern=".bio (.*)"))
+@tenth.on(events.NewMessage(incoming=True, pattern=".bio (.*)"))        
+async def _(event):
+    if event.fwd_from:
+        return
+    if event.sender_id not in SMEX_USERS:
+        return
+    bio = event.pattern_match.group(1)
+    await event.delete()
+    event = await edit_or_reply(event, "Trying")
+    try:
+        await event.client(functions.account.UpdateProfileRequest(about=bio))
+        await event.edit("Succesfully changed my profile bio")
+    except Exception as e:
+        await event.edit(str(e))        
+        
+        
+        
+        
+        
+        
 @idk.on(events.NewMessage(incoming=True, pattern=".spam"))
 async def spam(e):
     if e.sender_id in SUDO:
@@ -166,14 +330,18 @@ async def mspam(e):
 @wdk.on(events.NewMessage(incoming=True, pattern=".ping"))
 @hdk.on(events.NewMessage(incoming=True, pattern=".ping"))
 @sdk.on(events.NewMessage(incoming=True, pattern=".ping"))
+@sixth.on(events.NewMessage(incoming=True, pattern=".ping"))
+@seven.on(events.NewMessage(incoming=True, pattern=".ping"))
+@eight.on(events.NewMessage(incoming=True, pattern=".ping"))
+@ninth.on(events.NewMessage(incoming=True, pattern=".ping"))
+@tenth.on(events.NewMessage(incoming=True, pattern=".ping"))
 async def ping(e):
-    if e.sender_id in SUDO:
+    if e.sender_id in SMEX_USERS:
+        event = await edit_or_reply(event, "Pong!")
         start = datetime.now()
-        xxx = "PINGING..."
-        event = await e.reply(xxx, parse_mode=None, link_preview=None )
         end = datetime.now()
         ms = (end-start).microseconds / 1000
-        await event.edit(f"**PONG**: **{ms}**")
+        await event.edit(f"🤖 𝗣𝗼𝗻𝗴!\n`{ms} 𝗺𝘀")
 
 
 
