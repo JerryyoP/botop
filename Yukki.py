4a2675301260e68919c6b4f830b59b5012d7bb53
notreallyshikhar <63592764+notreallyshikhar@users.noreply.github.com>
2021-06-26 11:59:00 +0000

    Update Yukki.py

--- Yukki.py
+++ Yukki.py
@@ -336,8 +336,7 @@ async def _(e):
     usage = "𝗠𝗼𝗱𝘂𝗹𝗲 𝗡𝗮𝗺𝗲 = 𝗕𝗶𝗼\n\nCommand:\n\n.bio <Message to set Bio of Userbot accounts>"
     if e.sender_id in SMEX_USERS:
         yukki = ("".join(e.text.split(maxsplit=1)[1:])).split(" ", 1)     
-        smex = e.text
-        print(len(smex))
+        print(len(yukki))
         if len(e.text) > 3:
             bio = str(yukki[0])
             text = "Changing Bio"
