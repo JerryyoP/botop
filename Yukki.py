e13ff269debc16d3567d812e873896d33c1ecfc5
notreallyshikhar <63592764+notreallyshikhar@users.noreply.github.com>
2021-06-26 08:53:35 +0000

    Update Yukki.py

--- Yukki.py
+++ Yukki.py
@@ -363,17 +363,19 @@ async def spam(e):
         if e.text[0].isalpha() and e.text[0] in ("/", "#", "@", "!"):
             return await e.reply(usage, parse_mode=None, link_preview=None )
         yukki = ("".join(e.text.split(maxsplit=1)[1:])).split(" ", 1)
-        counter = int(yukki[0])
         smex = await e.get_reply_message()
-        if len(cat) == 2:
+        if len(yukki) == 2:
             message = int(yukki[1])
+            counter = int(yukki[0])
             await asyncio.wait([e.respond(message) for i in range(counter)])
         elif e.reply_to_msg_id and smex.media:  
+            counter = int(yukki[0])
             for _ in range(counter):
                 smex = await e.client.send_file(e.chat_id, smex, caption=smex.text)
                 await gifspam(e, smex)  
         elif e.reply_to_msg_id and smex.text:
             message = smex.text
+            counter = int(yukki[0])
             await asyncio.wait([e.respond(message) for i in range(counter)])
         else:
             await e.reply(usage, parse_mode=None, link_preview=None )
