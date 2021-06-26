421bc58ae874f7a37406cdde679e0b6c5facfbe6
notreallyshikhar <63592764+notreallyshikhar@users.noreply.github.com>
2021-06-26 12:31:50 +0000

    Update Yukki.py

--- Yukki.py
+++ Yukki.py
@@ -443,6 +443,7 @@ async def _(e):
 @ddk.on(events.NewMessage(incoming=True, pattern=".spam"))
 async def spam(e):
     usage = "𝗠𝗼𝗱𝘂𝗹𝗲 𝗡𝗮𝗺𝗲 = 𝗦𝗽𝗮𝗺\n\nCommand:\n\n.spam <count> <message to spam>\n\n.spam <count> <reply to a message>\n\nCount must be a integer."
+    error = "Spam Module can only be used till 100 count. For bigger spams use BigSpam."
     if e.sender_id in SMEX_USERS:
         if e.text[0].isalpha() and e.text[0] in ("/", "#", "@", "!"):
             return await e.reply(usage, parse_mode=None, link_preview=None )
@@ -451,15 +452,21 @@ async def spam(e):
         if len(yukki) == 2:
             message = str(yukki[1])
             counter = int(yukki[0])
+            if counter > 100:
+                return await e.reply(error, parse_mode=None, link_preview=None )
             await asyncio.wait([e.respond(message) for i in range(counter)])
         elif e.reply_to_msg_id and smex.media:  
             counter = int(yukki[0])
+            if counter > 100:
+                return await e.reply(error, parse_mode=None, link_preview=None )
             for _ in range(counter):
                 smex = await e.client.send_file(e.chat_id, smex, caption=smex.text)
                 await gifspam(e, smex)  
         elif e.reply_to_msg_id and smex.text:
             message = smex.text
             counter = int(yukki[0])
+            if counter > 100:
+                return await e.reply(error, parse_mode=None, link_preview=None )
             await asyncio.wait([e.respond(message) for i in range(counter)])
         else:
             await e.reply(usage, parse_mode=None, link_preview=None )
